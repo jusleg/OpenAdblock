661c8673a1866806fa02cc54f765ef12da3dc67e
jusleg <justin@Dany-Legers-MacBook-Pro.local>
2015-08-13 14:13:15 -0400

    initial commit

---
+++ Open Adblock/Open Adblock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Open Adblock
+//
+//  Created by Justin on 2015-08-13.
+//  Copyright © 2015 OpenAdblock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
